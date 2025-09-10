403d2b6a0af2b4974ff289dc0b3b67f536de0ac8
Rishika <rsrishika2007@gmail.com>
2025-09-10 17:40:49 +0530

    Add files via upload

---
+++ ALGORITHM.py
@@ -0,0 +1,9 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=[2,3,4,5,6]
+... i=2
+... sum=0
+... while len(a)>0:
+...     sum=sum+i
+...     i=i+1
+... print(sum)
